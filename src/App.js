49b297e87dcb975571365664cb6710b46fda0c97
Lavish <lavishjain36@gmail.com>
2022-08-03 15:35:10 +0530

    React crud

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Cards from "./components/Cards";
+import Footer from "./components/Footer";
+import data from "./data/data.json";
+import { useState } from "react";
+
+//Merging, creating and defining props and state variables for required component
+const App = () => {
+  
+  // counter state
+  const [itemCount, setItemCount] = useState(0);
+
+  //function to check type of button for calling related functions
+  const manageItems = (event) => {
+    if (event.target.innerText === "Add to cart") {
+      addItem(event);
+
+    } else if(event.target.innerText === "Remove from cart") {
+      removeItem(event);
+    }
+  }
+
+  //add item function to increase item count
+  const addItem = (event) => {
+    setItemCount(() => itemCount + 1);
+    event.target.innerText = "Remove from cart";
+    event.target.classList.value = "btn btn-outline-danger mt-auto";
+  };
+
+  //remove item function to decrease item count
+  const removeItem = (event) => {
+    setItemCount(() => itemCount - 1);
+    event.target.innerText = "Add to cart";
+    event.target.classList.value = "btn btn-outline-dark mt-auto";
+  };
+ 
+  return (
+    <>
+      <Navbar itemCount={itemCount} />
+      <Header />
+      <Cards data={data} manageItems={manageItems} />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+//creating and exporting footer component
+const Footer = () => {
+  return (
+    <footer className="py-5 bg-dark">
+      <div className="container">
+        <p className="m-0 text-center text-white">
+          Copyright &copy; Your Website 2022
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
